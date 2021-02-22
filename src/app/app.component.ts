0f58ff6896a09bbcf7b64b99d37f48e2f4fc1ae9
Kev James <kevin.james@sailpoint.com>
2021-02-22 09:58:14 +0000

    event poller

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,32 +18,61 @@ export class AppComponent implements OnInit {
   subscriptions = null;
   events: Event[] = [];
 
-  getSubscriptions() {
+  getConnections() {
     axios.get(httpUrl + "/connections").then(response => {
       this.connections = response.data;
     });
   }
 
-  ngOnInit() {
-    this.getSubscriptions();
-    axios.get(httpUrl + "/events").then(response => {
+  getSubscriptions() {
+    axios.get(httpUrl + "/subscriptions").then(response => {
+      this.subscriptions = response.data;
+    });
+  }
+
+
+  /** Get event list from the server */
+  getEvents(since) {
+
+    let url = httpUrl + "/events";
+    if (since) url += "?since=" + since;
+
+    return axios.get(url).then(response => {
       this.events = response.data;
       console.log(`events=${JSON.stringify(this.events)}`);
       if (this.events.length > 0) {
         this.lastEventTimestamp = this.events[this.events.length - 1].timestamp;
       }
     });
+  }
 
-    let subs = new Map<string, Subscription[]>();
-    subs.set("enterprise103", [
-      // new SubscriptionListComponent.subscription( checked: false, subscription: "IdentityCreated"),
-      //new subscription( checked: false, subscription: "IdentityDeleted")
-    ]);
-    subs.set("enterprise104", [
-      { checked: false, name: "AccessRequestSomething" },
-      { checked: false, name: "SomeOtherthing" }
-    ]);
-    this.subscriptions = subs;
-    console.log(`keys: ${JSON.stringify(this.subscriptions)}`);
+  // Get events, and then set up a timer to query every 5 seconds
+  setupEventsPoller() {
+
+    return this.getEvents(events => {
+      let timerId = setInterval(() => {
+        return this.getEvents(this.lastEventTimestamp);
+      }, 5000);
+    });
+
+  }
+
+  ngOnInit() {
+    this.getConnections();
+    this.getSubscriptions();
+
+    this.setupEventsPoller();
+
+    // let subs = new Map<string, Subscription[]>();
+    // subs.set("enterprise103", [
+    //   // new SubscriptionListComponent.subscription( checked: false, subscription: "IdentityCreated"),
+    //   //new subscription( checked: false, subscription: "IdentityDeleted")
+    // ]);
+    // subs.set("enterprise104", [
+    //   { checked: false, name: "AccessRequestSomething" },
+    //   { checked: false, name: "SomeOtherthing" }
+    // ]);
+    // this.subscriptions = subs;
+    // console.log(`keys: ${JSON.stringify(this.subscriptions)}`);
   }
 }
